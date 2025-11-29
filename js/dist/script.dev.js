5077a5f28fd5fc9cb3b7f72f1a00cfcedd1633b8
VladlenaKyrychenko <vlada.cosm@gmail.com>
2025-11-29 20:04:12 +0000

    home page carousel

---
+++ js/dist/script.dev.js
@@ -0,0 +1,16 @@
+"use strict";
+
+$('.owl-carousel').owlCarousel({
+  autoplay: true,
+  autoplayTimeout: 2500,
+  autoplaySpeed: 1000,
+  loop: true,
+  responsive: {
+    0: {
+      items: 1
+    },
+    1100: {
+      items: 1
+    }
+  }
+});
